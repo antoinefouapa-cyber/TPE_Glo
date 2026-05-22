43a54dbcbeb1ee67e47388011dd5688b2799635c
mrhal123 <lazarflam@gmail.com>
2026-05-22 10:46:36 +0100

    Add Enseignants class with constructor and fields

--- Enseignants.java
+++ Enseignants.java
@@ -1,1 +1,12 @@
-Enseignants.java
+public class Enseignants {
+    public int id;
+    public String nom;
+    public String prenom;
+
+    public Enseignants(int id, String nom, String prenom) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+}
+
