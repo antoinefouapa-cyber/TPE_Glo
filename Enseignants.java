acab663661b130b7ec12e0689800a47bcc28e35e
mrhal123 <lazarflam@gmail.com>
2026-05-31 22:46:06 +0100

    Delete Enseignants.java

--- Enseignants.java
+++
@@ -1,7 +0,0 @@
-Utilisateurs {   
-     public Enseignants(String nom, String prenom, String email, String motDePasse, Role role){ 
-            
-               super(nom, prenom, email, motDePasse, Role.ENSEIGNANT);    
-    }
-}
-
