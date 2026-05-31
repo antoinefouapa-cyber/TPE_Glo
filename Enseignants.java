225917de074be3b1c5394ae4149f90fcdc01d805
mrhal123 <lazarflam@gmail.com>
2026-05-31 22:35:01 +0100

    Ajout des classes

--- Enseignants.java
+++ Enseignants.java
@@ -1,23 +1,7 @@
-public class Enseignants {
-    private int id;
-    private String nom;
-    private String prenom;
-
-    // Constructeur
-    public Enseignants(int id, String nom, String prenom) {
-        this.id = id;
-        this.nom = nom;
-        this.prenom = prenom;
+Utilisateurs {   
+     public Enseignants(String nom, String prenom, String email, String motDePasse, Role role){ 
+            
+               super(nom, prenom, email, motDePasse, Role.ENSEIGNANT);    
     }
-
-    // getters et setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-
-    public String getNom() { return nom; }
-    public void setNom(String nom) { this.nom = nom; }
-
-    public String getPrenom() { return prenom; }
-    public void setPrenom(String prenom) { this.prenom = prenom; }
 }
 
